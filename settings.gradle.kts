2208154dd73393f4df7082b7f42b4ea15496de69
NT Woods (P) Ltd <mis01@ntwoods.com>
2026-09-16 13:23:05 +0530

    Rename Gradle project after encryption removal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "OfflineEncryptedVideoPlayer"
+rootProject.name = "NTWoodsOfflineMediaPlayer"
 include(":app")
